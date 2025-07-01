fc6dbe439fe3788190f54985f447d6b39bef0156
Justin Williams <justin@bamboodev.com>
2025-07-01 15:00:23 -0700

    added address

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -36,6 +36,30 @@
 			/>
 			<div class=" flex justify-center items-center">
 				<div class="p-6 text-white">
+					<div class="flex items-center mb-3">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5 mr-2 text-gray-400"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+							/>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+							/>
+						</svg>
+						<span class="text-white">21250 Hawthorne Blvd, Suite 500<br />Torrance, CA 90503</span>
+					</div>
+
 					<a href="mailto:hello@consilien.com" class="flex items-center mb-3">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
